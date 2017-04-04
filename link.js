5105c63f4c75293beebf6f7ed1f296eb58bab615
DAQIXIAOQI <qijiecongcong@163.com>
2017-04-04 15:12:13 +0800

    second

--- link.js
+++ link.js
@@ -1,71 +1,105 @@
-window.onload = function(){
-    
-     var newLink = createRandomList(10);
-    //随机生成长度为num的链表（不包括头部）
-    function createRandomList(num){
+window.onload = function () {
+
+    var newLink = createListByHead(10);
+    deleteList(newLink);
+
+    //线性表链式整表（长度为num）创建（头插法、尾插法） 
+    function createListByTail(num) {
         var list = createNode();
-        for(var i = 0 ; i < num ; i++){
-            insertItem(list,1,Math.round(Math.random()*10));
+        var pre = list ;
+        for (var i = 0; i < num; i++) {
+            var newnode = new LinkNode(i+1,pre.next);
+            pre.next = newnode;
+            list.data += 1 ;
+            pre = newnode;  
         }
         console.log("隨機生成的鏈表為:" + JSON.stringify(list));
-        return list 
+        return list
     }
-    
-    
-    function LinkNode(data,next){
-        this.data = data ;
-        this.next = next ;
+    function createListByHead(num) {
+        var list = createNode();
+       
+         for(var i = 0 ; i < num ; i++){
+             var newnode = new LinkNode(i+1,list.next);
+             list.next = newnode ;
+             list.data+=1;
+         }
+         console.log("隨機生成的鏈表為:" + JSON.stringify(list));
+        return list
     }
-    function createNode(){
-        var list = new LinkNode(0,null);
-        return list ;
-    }
-    function getItem(list,index){
-       var obj = list.next ;
-       var j = 1 ;
-       //一直循环直到index
-       while(obj&&j<index){
-           ++j;
-           obj=obj.next;
-       }
-       if(!obj||j>index){
-           console.log("元素不存在");
-           return
-       }
-       return obj ;    
-    }     
-    function insertItem(link,index,data){
-        var obj = link ;
-        var j = 1 ;
-        while(obj&&j<index){
-            j++;
-            obj=obj.next;
+    //线性表链式整表删除
+    function deleteList(list) {
+        var p , q ;
+        p = list.next;
+        
+        while(p){
+            q = p.next ;
+            delete p ;
+            p = q;
         }
-        while(!obj||j>index){
-            console.log("輸入的索引有誤");
-            return 
-        } 
-        var newItem = new LinkNode(data,obj.next);
-        obj.next = newItem ;
-        link.data += 1 ;
+        
+        list.next = null ;
+        list.data = 0 ;
+        console.log("鏈表為:" + JSON.stringify(list))
     }
-    //刪除第index個元素，返回值為刪除的元素
-    function deleteItem(link,index){
-        var j = 1 ;
-        var obj = link.next ;
-        while(obj.next&&j<index){
+
+    function LinkNode(data, next) {
+        this.data = data;
+        this.next = next;
+    }
+
+    function createNode() {
+        var list = new LinkNode(0, null);
+        return list;
+    }
+
+    function getItem(list, index) {
+        var obj = list.next;
+        var j = 1;
+        //一直循环直到index
+        while (obj && j < index) {
             ++j;
             obj = obj.next;
         }
-        if(!(obj.next)||j>index){
+        if (!obj || j > index) {
+            console.log("元素不存在");
+            return
+        }
+        return obj;
+    }
+
+    function insertItem(link, index, data) {
+        var obj = link;
+        var j = 1;
+        while (obj && j < index) {
+            j++;
+            obj = obj.next;
+        }
+        while (!obj || j > index) {
+            console.log("輸入的索引有誤");
+            return
+        }
+        var newItem = new LinkNode(data, obj.next);
+        obj.next = newItem;
+        link.data += 1;
+    }
+    //刪除第index個元素，返回值為刪除的元素
+    function deleteItem(link, index) {
+        var j = 1;
+        var obj = link.next;
+        while (obj.next && j < index) {
+            ++j;
+            obj = obj.next;
+        }
+        if (!(obj.next) || j > index) {
             console.log("要刪除的元素不存在");
             return
         }
-        
-        var item = obj.next ; 
+
+        var item = obj.next;
         obj.next = item.next;
-        link.data -= 1 ;
+        link.data -= 1;
         return item
     }
-    
+
 }
